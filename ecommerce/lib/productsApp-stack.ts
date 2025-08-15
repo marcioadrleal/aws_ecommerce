a8935ba66f728d52656d0c38804d0a46bd1dac72
marcioadrleal <marcioadrleal@gmail.com>
2025-08-14 22:08:16 -0300

    Atualização

---
+++ ecommerce/lib/productsApp-stack.ts
@@ -0,0 +1,20 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+
+import { Construct } from "constructs"
+
+export class ProductsAppStack extends cdk.Stack{
+
+    readonly productsFetchHandles : lambdaNodeJS.NodejsFunction;
+
+    constructor( scope : Construct , id : string , props?: cdk.StackProps){
+       super( scope , id , props);  
+       this.productsFetchHandles = new lambdaNodeJS.NodejsFunction(this,"ProductsFetchFunction",{
+         functionName : "ProductsFetchFunction" ,
+         entry: "lambda/products/productsFetchFunction.ts",
+         handler: "handler"
+       })   
+
+    }
+}
